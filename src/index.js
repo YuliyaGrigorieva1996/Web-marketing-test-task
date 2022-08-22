5fa02e7a40c1a23ad450ff2310c23f153e395e2e
Yuliya Hryhoryeva <yuliyahryhoryeva1996@gmail.com>
2022-08-22 15:52:13 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import './scss/style.scss';
+import './img/1.jpg';
+import './img/2.jpg';
+import './img/3.jpg';
+import './img/4.jpg';
+import './img/5.jpg';
+import './img/6.jpg';
+import './img/call.svg';
+import './img/copyright-solid.svg';
+import './img/facebook.svg';
+import './img/instagram.svg';
+import './img/location.svg';
+import './img/twitter.svg';
+import './img/youtube.svg';
+import './img/logo.png';
+
